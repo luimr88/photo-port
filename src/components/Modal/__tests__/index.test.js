67f4977325ad9b1e1b0777102d92b47b5ae0331b
Luis <luisriveros26@gmail.com>
2022-07-22 21:27:03 -0600

    added modal and test for modal

---
+++ src/components/Modal/__tests__/index.test.js
@@ -0,0 +1,50 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Modal from '..';
+
+const currentPhoto = {
+    name: 'Park bench',
+    category: 'landscape',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+    index: 1
+};
+
+const mockToggleModal = jest.fn();
+
+afterEach(cleanup);
+
+describe('Modal Component', () => {
+    it('renders', () => {
+        // baseline render component test
+        render(<Modal
+            currentPhoto={currentPhoto}
+            onClose={mockToggleModal}
+        />);
+    })
+    // snapshot test
+    it('matches snapshot DOM node structure', () => {
+        // Arrange the snapshot - declare variables
+        const { asFragment } = render(<Modal
+            currentPhoto={currentPhoto}
+            onClose={mockToggleModal}
+        />);
+        // Assert the match
+        expect(asFragment()).toMatchSnapshot();
+    });
+})
+
+describe('Click Event', () => {
+    it('calls onClose handler', () => {
+        // Arrange: Render Modal
+        const { getByText } = render(<Modal
+            onClose={mockToggleModal}
+            currentPhoto={currentPhoto}
+        />);
+        // Act: Simulate click event
+        fireEvent.click(getByText('Close this modal'));
+        // Assert: Expected matcher
+        expect(mockToggleModal).toHaveBeenCalledTimes(1);
+    });
+})  
